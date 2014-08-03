57f5bb8a245faaa0bce4e13afb9b7c028786e86b
Alexander Plavin <alexander@plav.in>
2014-08-03 23:24:50 +0400

    add 'exact' mode to Perplexity

--- py_artm/quantities/perplexity.py
+++ py_artm/quantities/perplexity.py
@@ -6,14 +6,20 @@ from ..plsa import QuantityBase
 
 class Perplexity(QuantityBase):
 
+    def __init__(self, exact=False):
+        self.exact = exact
+
     def _items(self, n, nwd, pwd):
-        s = np.einsum('ij -> ',
-                      ne.evaluate('nwd * (pwd_i * a + b)',
-                                  local_dict={'nwd': nwd,
-                                              'a': np.float32(8.2629582881927490e-8),
-                                              'b': np.float32(-87.989971088),
-                                              'pwd_i': pwd.view(np.int32)},
-                                  casting='unsafe'))
-        # s = perplexity_internal_cython(nwd.size, pwd, nwd)
+        if self.exact:
+            s = ne.evaluate('where(nwd == 0, 0, nwd * log(pwd))').sum()
+        else:
+            mat = ne.evaluate('nwd * (pwd_i * a + b)',
+                        local_dict={'nwd': nwd,
+                                    'a': np.float32(8.2629582881927490e-8),
+                                    'b': np.float32(-87.989971088),
+                                    'pwd_i': pwd.view(np.int32)},
+                        casting='unsafe')
+            s = np.einsum('ij -> ', mat)
+            # s = perplexity_internal_cython(nwd.size, pwd, nwd)
         # TODO: check if smth like mat[self.nwd == 0] = 0 is needed
         yield ('perplexity', math.exp(-1/n * s))
