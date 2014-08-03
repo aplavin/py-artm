b1a99d7884f9189c1ec7f3664c84124c8b267b0b
Alexander Plavin <alexander@plav.in>
2014-08-03 22:22:13 +0400

    add ZeroThenConstant

--- py_artm/regularizers/coefficients.py
+++ py_artm/regularizers/coefficients.py
@@ -8,3 +8,13 @@ class Constant(RegularizerCoefficientBase):
 
     def _coefficient(self):
         return self.value
+
+
+class ZeroThenConstant(RegularizerCoefficientBase):
+
+    def __init__(self, steps_zero, value):
+    	self.steps_zero = steps_zero
+        self.value = value
+
+    def _coefficient(self, itnum):
+        return self.value if itnum >= self.steps_zero else 0
