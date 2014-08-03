9f979f7d057a33c92d46bf2afee012f3fe6f052f
Alexander Plavin <alexander@plav.in>
2014-08-03 22:56:52 +0400

    add imports

--- py_artm/plsa/__init__.py
+++ py_artm/plsa/__init__.py
@@ -1,2 +1,4 @@
 from .rational_em import PlsaEmRational
 from .regularizer import RegularizerBase, RegularizerCoefficientBase
+from .quantity import QuantityBase
+from .stop_condition import StopConditionBase
