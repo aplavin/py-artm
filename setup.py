114aefbeeabc7d8dead528daceab04e98e9edf6c
Alexander Plavin <alexander@plav.in>
2014-08-03 13:46:48 +0400

    initial package structure

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    long_description = ''
+
+setup(name='py-artm',
+      version='0.1',
+      description='',
+      long_description = long_description,
+      classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
+      keywords='',
+      url='',
+      author='Alexander Plavin',
+      author_email='alexander@plav.in',
+      license='MIT',
+      packages=['py_artm'],
+      zip_safe=False)
