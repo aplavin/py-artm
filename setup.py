a0edbbdabac9b5c6f58b7ca446cd472ff204bae5
Alexander Plavin <alexander@plav.in>
2014-10-10 20:30:21 +0400

    add all packages and dependecies to setup.py

--- setup.py
+++ setup.py
@@ -26,5 +26,6 @@ setup(name='py-artm',
       author='Alexander Plavin',
       author_email='alexander@plav.in',
       license='MIT',
-      packages=['py_artm', 'py_artm.plsa', 'py_artm.regularizers'],
+      packages=['py_artm', 'py_artm.plsa', 'py_artm.regularizers', 'py_artm.quantities', 'py_artm.stop_conditions'],
+      install_requires=[ 'numpy', 'scipy', 'numexpr', 'ipy-progressbar' ],
       zip_safe=False)
