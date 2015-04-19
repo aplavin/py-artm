866aafaaea680c6f68486363722e88ee68f80c7d
Alexander Plavin <alexander@plav.in>
2015-04-19 20:32:15 +0300

    manifest and setup to include all files

--- setup.py
+++ setup.py
@@ -26,6 +26,6 @@ setup(name='py-artm',
       author='Alexander Plavin',
       author_email='alexander@plav.in',
       license='MIT',
-      packages=['py_artm', 'py_artm.plsa', 'py_artm.regularizers', 'py_artm.quantities', 'py_artm.stop_conditions'],
+      packages=['py_artm', 'py_artm.plsa', 'py_artm.regularizers', 'py_artm.regularizers.coefficients', 'py_artm.quantities', 'py_artm.stop_conditions'],
       install_requires=[ 'numpy', 'scipy', 'numexpr', 'ipy-progressbar' ],
       zip_safe=False)
